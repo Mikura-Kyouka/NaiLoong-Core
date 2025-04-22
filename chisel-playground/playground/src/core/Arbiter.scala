7961d15ee4ff1bb8648cbbe4b658b7ffc3946bb4
Mikura-Kyouka <13207280@qq.com>
2025-04-22 15:03:22 +0800

    fix arbiter FSM logic

--- chisel-playground/playground/src/core/Arbiter.scala
+++ chisel-playground/playground/src/core/Arbiter.scala
@@ -13,39 +13,66 @@ class Arb extends Module {
   val s_idle :: s_ifu :: s_lsu_write :: s_lsu_read :: Nil = Enum(4)
   val state = RegInit(s_idle)
 
-  state := MuxLookup(state, s_idle)(
-    List(
-      // s_idle -> Mux(
-      //   io.ifu.arvalid,
-      //   s_ifu,
-      //   Mux(
-      //     io.lsu.awvalid & io.lsu.wvalid,
-      //     s_lsu_write,
-      //     Mux(io.lsu.arvalid, s_lsu_read, s_idle)
-      //   )
-      // ),
-      s_idle -> Mux(
-        io.lsu.awvalid & io.lsu.wvalid,
-        s_lsu_write,
-        Mux(
-          io.lsu.arvalid,
-          s_lsu_read,
-          Mux(io.ifu.arvalid, s_ifu, s_idle)
-        )
-      ),
-      s_ifu -> Mux(io.ifu.rlast, s_idle, s_ifu),
-      s_lsu_write -> Mux(
-        io.lsu.bvalid & io.lsu.bready,
-        s_idle,
-        s_lsu_write
-      ),
-      s_lsu_read -> Mux(
-        io.lsu.rvalid & io.lsu.rready,
-        s_idle,
-        s_lsu_read
-      )
-    )
-  )
+  // state := MuxLookup(state, s_idle)(
+  //   List(
+  //     // s_idle -> Mux(
+  //     //   io.ifu.arvalid,
+  //     //   s_ifu,
+  //     //   Mux(
+  //     //     io.lsu.awvalid & io.lsu.wvalid,
+  //     //     s_lsu_write,
+  //     //     Mux(io.lsu.arvalid, s_lsu_read, s_idle)
+  //     //   )
+  //     // ),
+  //     s_idle -> Mux(
+  //       io.lsu.awvalid & io.lsu.wvalid,
+  //       s_lsu_write,
+  //       Mux(
+  //         io.lsu.arvalid,
+  //         s_lsu_read,
+  //         Mux(io.ifu.arvalid, s_ifu, s_idle)
+  //       )
+  //     ),
+  //     s_ifu -> Mux(io.ifu.rlast, s_idle, s_ifu),
+  //     s_lsu_write -> Mux(
+  //       io.lsu.bvalid & io.lsu.bready,
+  //       s_idle,
+  //       s_lsu_write
+  //     ),
+  //     s_lsu_read -> Mux(
+  //       io.lsu.rvalid & io.lsu.rready & io.lsu.rlast,
+  //       s_idle,
+  //       s_lsu_read
+  //     )
+  //   )
+  // )
+
+  switch(state) {
+    is(s_idle) {
+      when(io.lsu.awvalid && io.lsu.wvalid) {
+        state := s_lsu_write
+      }.elsewhen(io.lsu.arvalid) {
+        state := s_lsu_read
+      }.elsewhen(io.ifu.arvalid) {
+        state := s_ifu
+      }
+    }
+    is(s_ifu) {
+      when(io.out.rvalid && io.out.rready && io.out.rlast) {
+        state := s_idle
+      }
+    }
+    is(s_lsu_write) {
+      when(io.out.bvalid && io.out.bready) {
+        state := s_idle
+      }
+    }
+    is(s_lsu_read) {
+      when(io.out.rvalid && io.out.rready && io.out.rlast) {
+        state := s_idle
+      }
+    }
+  }
 
   /* Read transection */
   io.out.arid := 0.U
@@ -63,7 +90,7 @@ class Arb extends Module {
   io.ifu.rvalid := 0.U
   io.ifu.rdata := 0.U
   io.ifu.rresp := 0.U
-  io.ifu.rlast := false.B
+  io.ifu.rlast := true.B
 
   io.ifu.awready := 0.U
   io.ifu.wready := 0.U
@@ -113,15 +140,21 @@ class Arb extends Module {
     io.out.arvalid := io.lsu.arvalid
     io.out.araddr := io.lsu.araddr
     io.out.arsize := io.lsu.arsize //
+    io.out.arlen := io.lsu.arlen 
+    io.out.arburst := io.lsu.arburst
     io.out.rready := io.lsu.rready
 
     io.lsu.arready := io.out.arready
     io.lsu.rvalid := io.out.rvalid
+    io.lsu.rlast := io.out.rlast
+    io.lsu.rid := io.out.rid 
   }.elsewhen(state === s_lsu_write) {
     io.out.awvalid := io.lsu.awvalid
     io.out.wvalid := io.lsu.wvalid
     io.out.awaddr := io.lsu.awaddr
     io.out.awsize := io.lsu.awsize //
+    io.out.awlen := io.lsu.awlen
+    io.out.awburst := io.lsu.awburst
     io.out.wdata := io.lsu.wdata
     io.out.wstrb := io.lsu.wstrb
     io.out.bready := io.lsu.bready
@@ -141,10 +174,12 @@ class Arb extends Module {
 
     io.ifu.arready := false.B
     io.ifu.rvalid := false.B
+    io.ifu.bvalid := false.B
     io.lsu.arready := false.B
     io.lsu.rvalid := false.B
     io.lsu.awready := false.B
     io.lsu.wready := false.B
+    io.lsu.bvalid := false.B
   }
   /* duplicate single input to multiple outputs */
   io.lsu.rdata := io.out.rdata
