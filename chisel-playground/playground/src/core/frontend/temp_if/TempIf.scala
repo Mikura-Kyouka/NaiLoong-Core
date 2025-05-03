1efe4b87c7ed8ce2e651b4b4eda384a465e4cdc5
Mikura-Kyouka <13207280@qq.com>
2025-05-03 19:22:23 +0800

    fix icache doesn't paused by flush

--- chisel-playground/playground/src/core/frontend/temp_if/TempIf.scala
+++ chisel-playground/playground/src/core/frontend/temp_if/TempIf.scala
@@ -74,6 +74,18 @@ class TempIf extends Module {
 
   val pc = RegInit("h1c000000".U(32.W))
 
+  // 解决非16字节对齐的问题
+  val use_cached_pc = RegInit(false.B)
+  when(io.flush) {
+    use_cached_pc := true.B
+  }.elsewhen(io.to.ready && io.to.valid) {
+    use_cached_pc := false.B
+  }
+  val cached_new_pc = RegInit(0.U(32.W))
+  when(io.flush) {
+    cached_new_pc := io.new_pc
+  }
+
   val idle :: read :: waitar :: waitr :: Nil = Enum(4)
   val state = RegInit(idle)
 
@@ -136,21 +148,21 @@ class TempIf extends Module {
   temp_cache.io.wen := cache_wen
   temp_cache.io.new_pc := io.new_pc
   temp_cache.io.flush := io.flush
-  temp_cache.io.ready := io.to.ready
+  temp_cache.io.ready := !io.flush
   io.to.valid := temp_cache.io.valid
 
   io.to.bits(0).instr := temp_cache.io.inst0.inst
   io.to.bits(0).pc := temp_cache.io.inst0.pc
-  io.to.bits(0).valid := temp_cache.io.inst0.valid
+  io.to.bits(0).valid := temp_cache.io.inst0.valid && (!use_cached_pc || io.to.bits(0).pc(5, 4) >= cached_new_pc(5, 4))
   io.to.bits(1).instr := temp_cache.io.inst1.inst
   io.to.bits(1).pc := temp_cache.io.inst1.pc
-  io.to.bits(1).valid := temp_cache.io.inst1.valid
+  io.to.bits(1).valid := temp_cache.io.inst1.valid && (!use_cached_pc || io.to.bits(1).pc(5, 4) >= cached_new_pc(5, 4))
   io.to.bits(2).instr := temp_cache.io.inst2.inst
   io.to.bits(2).pc := temp_cache.io.inst2.pc
-  io.to.bits(2).valid := temp_cache.io.inst2.valid
+  io.to.bits(2).valid := temp_cache.io.inst2.valid && (!use_cached_pc || io.to.bits(2).pc(5, 4) >= cached_new_pc(5, 4))
   io.to.bits(3).instr := temp_cache.io.inst3.inst
   io.to.bits(3).pc := temp_cache.io.inst3.pc
-  io.to.bits(3).valid := temp_cache.io.inst3.valid
+  io.to.bits(3).valid := temp_cache.io.inst3.valid && (!use_cached_pc || io.to.bits(3).pc(5, 4) >= cached_new_pc(5, 4))
 
   val flushed = RegInit(false.B)
   when(io.flush || temp_cache.io.read_pc(31, 4) =/= pc(31, 4)) {
