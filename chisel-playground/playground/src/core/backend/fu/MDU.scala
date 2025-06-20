a6c206655f08699beb7f70152407d540ba32b903
Mikura-Kyouka <13207280@qq.com>
2025-06-20 21:30:57 +0800

    fix mdu out valid

--- chisel-playground/playground/src/core/backend/fu/MDU.scala
+++ chisel-playground/playground/src/core/backend/fu/MDU.scala
@@ -62,8 +62,8 @@ class AlignedMDU extends Module{
   val out_valid = RegInit(false.B)
 
   io.in.ready := in_ready && (!io.in.valid || io.out.fire)
-  io.out.valid := out_valid 
-
+  io.out.valid := out_valid && io.in.bits.valid
+  
   val idle :: put :: waiting :: get :: Nil = Enum(4)
   val div = RegInit(0.U(32.W))
   val divu = RegInit(0.U(32.W))
