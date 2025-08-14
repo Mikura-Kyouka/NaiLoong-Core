99fe8bd655947ba979b26e2f95b991231d33b36b
Mikura Kyouka <13207280@qq.com>
2025-08-14 16:45:02 +0800

    Fix dcacop again

--- chisel-playground/playground/src/core/backend/ooo/DCache.scala
+++ chisel-playground/playground/src/core/backend/ooo/DCache.scala
@@ -209,7 +209,7 @@ class DCache(implicit val cacheConfig: DCacheConfig) extends CacheModule{
                             Mux(cacopOp2, Mux(dirty, s_write_mem1, s_idle), Mux(req.cmd, s_write_cache, s_read_cache)), 
                             Mux(!cacopOp2, Mux(dirty, s_write_mem1, s_read_mem1), s_idle))), 
         s_write_mem1 -> Mux(io.axi.awready, s_write_mem2, s_write_mem1),
-        s_write_mem2 -> Mux((io.axi.wready && (wburst === 3.U || isMMIO || cacopOp1 || cacopOp2)), s_write_mem3, s_write_mem2),
+        s_write_mem2 -> Mux((io.axi.wready && (wburst === 3.U || (isMMIO && !cacopOp1 && !cacopOp2))), s_write_mem3, s_write_mem2),
         s_write_mem3 -> Mux(io.axi.bvalid, Mux(isMMIO || cacopOp1 || cacopOp2, s_idle, s_read_mem1), s_write_mem3),
         s_read_mem1 -> Mux(io.axi.arready, s_read_mem2, s_read_mem1),
         s_read_mem2 -> Mux((io.axi.rvalid && io.axi.rlast), Mux(isMMIO || cacopOp1 || cacopOp2, s_idle, Mux(req.cmd, s_write_cache, s_read_cache)), s_read_mem2), // FIXME:
