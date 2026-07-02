36f74b35bc06ef93df8eaeccb82570c86b999f4f
Mikura Kyouka <13207280@qq.com>
2026-07-02 10:51:09 +0800

    Fix ICache cacop issue

--- chisel-playground/playground/src/core/frontend/ICache.scala
+++ chisel-playground/playground/src/core/frontend/ICache.scala
@@ -483,7 +483,7 @@ class Stage2(implicit val cacheConfig: ICacheConfig) extends ICacheModule {
     dataArray.io.wea := io.in.bits.mat === 1.U // 一致可缓存
     dataArray.io.dina := Cat(rdata, axiDataLatch(2), axiDataLatch(1), axiDataLatch(0))
     // metaArray update
-    io.metaArrayWrite.valid := io.in.bits.mat === 1.U // 一致可缓存
+    io.metaArrayWrite.valid := io.in.bits.mat === 1.U && !io.in.bits.isCACOP // 一致可缓存
     io.metaArrayWrite.index := index
     io.metaArrayWrite.tag := tag
   }
